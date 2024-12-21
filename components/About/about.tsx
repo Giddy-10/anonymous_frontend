5dec8b9d035a908177bfba90323ad49a8a36bf0e
MuthonduG <muthondugithinji@gmail.com>
2024-12-21 03:17:37 -0500

    updates: updates for about component testing width

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -9,64 +9,68 @@ import { FcInvite } from "react-icons/fc";
 
 const About = () => {
   return (
-    <Section style='flex flex-col justify-center items-center gap-2 p-4' color='' width='[70%]'>
-      <div className="flex justify-center items-center gap-3 w-[90%]">
+    <Section style="flex flex-col justify-center items-center gap-6 p-4" color="" width="">
+      {/* Content Wrapper */}
+      <div className="max-w-[80%] w-full flex justify-center items-center gap-6 mx-auto">
+        {/* Left Cards */}
         <div className="flex flex-col gap-3 flex-1">
-            <Card style=''>
-                <div className="flex justify-center items-center gap-4 p-4">
-                    <FcSmartphoneTablet className='text-3xl'/>
-                    <span><strong>Accessibility</strong></span>
-                </div>
-                <hr />
-                <div className="flex justify-center items-center p-2 text-center mb-3">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reprehenderit, doloribus ipsam eaque ea unde amet voluptatem expedita, velit pariatur odio commodi ut eum culpa sit vero nulla possimus delectus.
-                    </p>
-                </div>
-            </Card>
-            <Card style=''>
-                <div className="flex justify-center items-center gap-4 p-4">
-                    <FcPlus className='text-3xl'/>
-                    <span><strong>Assured Solutions</strong></span>
-                </div>
-                <hr />
-                <div className="flex justify-center items-center p-2 text-center mb-3">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reprehenderit, doloribus ipsam eaque ea unde amet voluptatem expedita, velit pariatur odio commodi ut eum culpa sit vero nulla possimus delectus.
-                    </p>
-                </div>
-            </Card>
+          <Card style="">
+            <div className="flex justify-center items-center gap-4 p-4">
+              <FcSmartphoneTablet className="text-3xl" />
+              <span><strong>Accessibility</strong></span>
+            </div>
+            <hr />
+            <div className="flex justify-center items-center p-2 text-center mb-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reprehenderit, doloribus ipsam eaque ea unde amet voluptatem expedita, velit pariatur odio commodi ut eum culpa sit vero nulla possimus delectus.
+              </p>
+            </div>
+          </Card>
+          <Card style="">
+            <div className="flex justify-center items-center gap-4 p-4">
+              <FcPlus className="text-3xl" />
+              <span><strong>Assured Solutions</strong></span>
+            </div>
+            <hr />
+            <div className="flex justify-center items-center p-2 text-center mb-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reprehenderit, doloribus ipsam eaque ea unde amet voluptatem expedita, velit pariatur odio commodi ut eum culpa sit vero nulla possimus delectus.
+              </p>
+            </div>
+          </Card>
         </div>
 
+        {/* Center Image */}
         <div className="flex flex-col gap-3 flex-1 rounded-xl overflow-hidden">
-            <Image src={AboutImage} alt='' className="h-[27rem] object-cover"/>
+          <Image src={AboutImage} alt="" className="h-[27rem] object-cover" />
         </div>
 
+        {/* Right Cards */}
         <div className="flex flex-col gap-3 flex-1">
-            <Card style=''>
-                <div className="flex justify-center items-center gap-4 p-4">
-                    <FcInvite className='text-3xl'/>
-                    <span><strong>Assured response</strong></span>
-                </div>
-                <hr />
-                <div className="flex justify-center items-center p-2 text-center mb-3">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reprehenderit, doloribus ipsam eaque ea unde amet voluptatem expedita, velit pariatur odio commodi ut eum culpa sit vero nulla possimus delectus.
-                    </p>
-                </div>
-            </Card>
-            <Card style=''>
-                <div className="flex justify-center items-center gap-4 p-4">
-                  <FcInvite className='text-3xl'/>
-                  <span><strong>Assured response</strong></span>
-                </div>
-                <hr />
-                <div className="flex justify-center items-center p-2 text-center mb-3">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reprehenderit, doloribus ipsam eaque ea unde amet voluptatem expedita, velit pariatur odio commodi ut eum culpa sit vero nulla possimus delectus.
-                    </p>
-                </div>
-            </Card>
+          <Card style="">
+            <div className="flex justify-center items-center gap-4 p-4">
+              <FcInvite className="text-3xl" />
+              <span><strong>Assured response</strong></span>
+            </div>
+            <hr />
+            <div className="flex justify-center items-center p-2 text-center mb-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reprehenderit, doloribus ipsam eaque ea unde amet voluptatem expedita, velit pariatur odio commodi ut eum culpa sit vero nulla possimus delectus.
+              </p>
+            </div>
+          </Card>
+          <Card style="">
+            <div className="flex justify-center items-center gap-4 p-4">
+              <FcInvite className="text-3xl" />
+              <span><strong>Assured response</strong></span>
+            </div>
+            <hr />
+            <div className="flex justify-center items-center p-2 text-center mb-3">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reprehenderit, doloribus ipsam eaque ea unde amet voluptatem expedita, velit pariatur odio commodi ut eum culpa sit vero nulla possimus delectus.
+              </p>
+            </div>
+          </Card>
         </div>
       </div>
     </Section>
