92cc3ff55f85d40f342de2024ea46f795259721c
MuthonduG <muthondugithinji@gmail.com>
2024-12-30 11:43:32 -0500

    updates: register page

--- app/pages/register/page.tsx
+++ app/pages/register/page.tsx
@@ -3,41 +3,42 @@ import React from 'react'
 import FormComponent from '@/components/ui/form'
 import Button from '@/components/ui/button';
 import { MdOutlineMail } from 'react-icons/md';
-import { LuCircleUserRound } from "react-icons/lu";
-import { LiaTelegramPlane } from "react-icons/lia";
-import { MdContentCopy } from "react-icons/md";
+import { MdPassword } from "react-icons/md";
+import { FaUserSecret } from "react-icons/fa";
 import RegisterImage from '@/public/assets/privacy3.gif'
 import Image from 'next/image';
 
 const formFields = [
-  { icon: <LuCircleUserRound className="w-5 h-5 text-gray-500 dark:text-gray-400" />, placeholder: 'Your Name', type: 'text' },
   { icon: <MdOutlineMail className="w-5 h-5 text-gray-500 dark:text-gray-400" />, placeholder: 'Your Email', type: 'email' },
-  { icon: <LiaTelegramPlane className="w-5 h-5 text-gray-500 dark:text-gray-400" />, placeholder: 'Subject Issue', type: 'text' },
+  { icon: <MdPassword className="w-5 h-5 text-gray-500 dark:text-gray-400" />, placeholder: 'Enter Password', type: 'password' },
+  { icon: <MdPassword className="w-5 h-5 text-gray-500 dark:text-gray-400" />, placeholder: 'Re-enter Password', type: 'password' },
+  { icon: <FaUserSecret className="w-5 h-5 text-gray-500 dark:text-gray-400" />, placeholder: 'Enter security phrase eg pet\s name', type: 'text' },
 ];
 
 const page = () => {
   return (
     <Section color='' width='[80%]' style='flex justify-center items-center'>
-      <div className="mt-20 p-10 flex items-center justify-center w-full gap-4">
-        <div className="">
-          <Image src={RegisterImage} alt=''/>
+      <div className="mt-40 p-10 grid grid-cols-2 items-center justify-center gap-4 shadow-2xl	bg-slate-100 rounded-xl px-10 w-[70%]">
+        <div className="h-[100%]">
+          <Image src={RegisterImage} alt='' className='h-[100%]'/>
         </div>
-        <FormComponent formFields={formFields} className={'w-full'}>
-            <div className="relative flex justify-center items-center mt-3">
-                <div className="absolute inset-y-0 left-0 flex pl-3 pointer-events-none">
-                  <MdContentCopy className='text-slate-400 text-xl mt-3'/>
-                </div>
-                <textarea
-                  placeholder='Describe The Issue'
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full h-[10rem] pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                />
-            </div>
-            <div className="flex justify-center items-center p-4">
+        <div className="flex flex-col items-center justify-center">
+          <div className="p-4">
+            <span className='text-2xl italic'>
+              <strong>Sign Up</strong>
+            </span>
+          </div>
+          <FormComponent formFields={formFields} className={'w-full'}>
+            <div className="flex justify-center items-center p-4 gap-4">
               <Button style='border-2 border-red-600 text-slate-700 hover:bg-red-500 hover:text-white' href=''>
-                Contact Us
+                Sign Up
+              </Button>
+              <Button style='border-2 border-red-600 text-slate-700 hover:bg-red-500 hover:text-white ' href=''>
+                Log In
               </Button>
             </div>
           </FormComponent>
+        </div>
       </div>
     </Section>
   )
